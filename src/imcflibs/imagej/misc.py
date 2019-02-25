92b9b2f03e876d57f16c0da651ea7ad96f14f0ce
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2019-02-25 17:07:25 +0100

    Use logger from log module instead of being a parameter
    It's an API breaking change, so that's a major release then. Sigh.

--- src/imcflibs/imagej/misc.py
+++ src/imcflibs/imagej/misc.py
@@ -2,14 +2,16 @@
 
 from ij import IJ  # pylint: disable-msg=E0401
 
-def show_status(logger, msg):
+from ..log import LOG as log
+
+def show_status(msg):
     """Wrapper to update the ImageJ status bar and the log simultaneously."""
-    logger.info(msg)
+    log.info(msg)
     IJ.showStatus(msg)
 
 
-def show_progress(logger, cur, final):
+def show_progress(cur, final):
     """Wrapper to update the progress bar and issue a log message."""
     # ij.IJ.showProgress is adding 1 to the value given as first parameter...
-    logger.info("Progress: %s / %s (%s)" % (cur+1, final, (1.0+cur)/final))
+    log.info("Progress: %s / %s (%s)", cur+1, final, (1.0+cur)/final)
     IJ.showProgress(cur, final)
