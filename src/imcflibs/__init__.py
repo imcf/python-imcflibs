64cfb3a355e2b175d1bae33b43880ee2688383e5
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2021-04-08 11:41:06 +0200

    Import submodules automatically

--- src/imcflibs/__init__.py
+++ src/imcflibs/__init__.py
@@ -22,3 +22,14 @@ Biozentrum, University of Basel, Switzerland.
 """
 
 __version__ = "${project.version}"
+
+from . import iotools
+from . import log
+from . import pathtools
+from . import strtools
+
+# check if we're running in Jython, then also import the 'imagej' submodule:
+import platform as _python_platform
+if _python_platform.python_implementation() == "Jython":
+    from . import imagej
+del _python_platform
