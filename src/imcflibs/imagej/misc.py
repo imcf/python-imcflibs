2101dd0ac3770046d75f6e2704ac44dc7becbd1a
Laurent Guerard <laurent.guerard@unibas.ch>
2022-08-18 09:40:22 +0200

    Add functions for elapsed time and mean/stdv

--- src/imcflibs/imagej/misc.py
+++ src/imcflibs/imagej/misc.py
@@ -1,6 +1,6 @@
 """Miscellaneous ImageJ related functions, mostly convenience wrappers."""
 
-import sys
+import sys, time
 from ij import IJ  # pylint: disable-msg=E0401
 
 from ..log import LOG as log
@@ -23,3 +23,62 @@ def error_exit(msg):
     """Convenience wrapper to log an error and exit then."""
     log.error(msg)
     sys.exit(msg)
+
+def elapsed_time_since(start,end=None):
+    """Prints the elapsed time for execution
+
+    Parameters
+    ----------
+    start : time
+        Start time
+    end : time, optional
+        End time
+
+    Returns
+    -------
+    str
+        Formatted time elapsed
+    """
+
+    if not end:
+        end = time.time()
+
+    hours, rem = divmod(end-start, 3600)
+    minutes, seconds = divmod(rem, 60)
+    return "{:0>2}:{:0>2}:{:05.2f}".format(int(hours),int(minutes),seconds)
+
+def percentage(part, whole):
+    """Returns the percentage of a value based on total
+
+    Parameters
+    ----------
+    part : float
+        Part
+    whole : float
+        Complete size
+
+    Returns
+    -------
+    float
+        Percentage
+    """
+    return 100 * float(part)/float(whole)
+
+def calculate_mean_and_stdv(list):
+    """Gets statistics from a list
+
+    Parameters
+    ----------
+    list : float
+        List containing values
+
+    Returns
+    -------
+    [float, float]
+        Mean and Std of the list
+    """
+    mean = sum(list)/len(list)
+    tot = 0.0
+    for x in list:
+        tot = tot + (x - mean)**2
+    return [mean, (tot/(len(list)))**0.5]
