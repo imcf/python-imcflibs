5aa604b9b39f6a41f591ba540eb5ea9d979d9391
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2023-02-21 18:55:52 +0100

    WIP: Rename parameter to "label_image", update docstring

--- src/imcflibs/imagej/labelimage.py
+++ src/imcflibs/imagej/labelimage.py
@@ -7,40 +7,43 @@ from ij.process import FloatProcessor
 from ij.plugin.filter import ThresholdToSelection, ImageProcessor
 
 
-def label_image_to_roi_list(implus, low_thresh=None):
+def label_image_to_roi_list(label_image, low_thresh=None):
     """Converts a label image to a list of ROIs
 
     Parameters
     ----------
-    implus : ImagePlus
+    label_image : ImagePlus
         Label image to convert
-    low_thresh : int
-        Value under which the labels should be discarded
+    low_thresh : int, optional
+        Value under which the labels should be discarded, by default None
 
     Returns
     -------
-    roi_list : ROI[]
+    roi_list : list(roi)  FIXME: what's the exact "roi" type?
         List of all the ROIs converted from the label image
     """
+
     roi_list = []
     max_value = 0
 
-    for slice in range(1, implus.getNSlices() + 1):
-        implus_slice = Duplicator().run(implus, 1, 1, slice, slice, 1, 1)
+    for slice in range(1, label_image.getNSlices() + 1):
+        label_image_slice = Duplicator().run(label_image, 1, 1, slice, slice, 1, 1)
 
-        ip = implus_slice.getProcessor()
-        pixels = ip.getFloatArray()
+        image_processor = label_image_slice.getProcessor()
+        pixels = image_processor.getFloatArray()
 
         existing_pixel_values = set()
 
-        for x in range(0, ip.getWidth()):
-            for y in range(0, ip.getHeight()):
+        for x in range(0, image_processor.getWidth()):
+            for y in range(0, image_processor.getHeight()):
                 existing_pixel_values.add(pixels[x][y])
 
         # Converts data in case it's RGB image
-        fp = FloatProcessor(ip.getWidth(), ip.getHeight())
-        fp.setFloatArray(pixels)
-        img_float_copy = ImagePlus("FloatLabel", fp)
+        float_processor = FloatProcessor(
+            image_processor.getWidth(), image_processor.getHeight()
+        )
+        float_processor.setFloatArray(pixels)
+        img_float_copy = ImagePlus("FloatLabel", float_processor)
 
         # print(existing_pixel_values)
         for value in existing_pixel_values:
@@ -50,7 +53,7 @@ def label_image_to_roi_list(implus, low_thresh=None):
             elif value == 0:
                 continue
             # print(value)
-            fp.setThreshold(value, value, ImageProcessor.NO_LUT_UPDATE)
+            float_processor.setThreshold(value, value, ImageProcessor.NO_LUT_UPDATE)
             roi = ThresholdToSelection.run(img_float_copy)
             roi.setName(str(value))
             roi.setPosition(slice)
