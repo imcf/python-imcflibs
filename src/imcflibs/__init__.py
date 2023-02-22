530dee63c9b4ba7aba56a4121dbd2919872fd53c
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2023-02-22 12:44:15 +0100

    Include README instead of duplicating stuff

--- src/imcflibs/__init__.py
+++ src/imcflibs/__init__.py
@@ -1,24 +1,6 @@
-"""A collection of Python helper functions.
+"""Python convenience from and for the IMCF (Imaging Core Facility).
 
-This package contains a diverse collection of Python functions dealing with
-paths, I/O (file handles, ...), strings etc.
-
-In addition (to the regular packaging) it is set up to be packaged by Maven to
-be ready for being used in [ImageJ2][imagej].
-
-Developed and provided by the [Imaging Core Facility (IMCF)][imcf] of the
-Biozentrum, University of Basel, Switzerland.
-
-# Contents
-
-:note: TODO!
-
-# Example usage
-
-:note: TODO!
-
-[imcf]: https://www.biozentrum.unibas.ch/imcf
-[imagej]: https://imagej.net
+.. include:: ../../README.md
 """
 
 __version__ = "${project.version}"
