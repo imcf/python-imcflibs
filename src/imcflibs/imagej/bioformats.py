53abdb18472cc07b89200ef834050af58c407c8b
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2023-03-21 18:47:03 +0100

    Snake-case for naming variables

--- src/imcflibs/imagej/bioformats.py
+++ src/imcflibs/imagej/bioformats.py
@@ -219,8 +219,8 @@ def get_series_count_from_ome_metadata(path_to_file):
         The number of Bio-Formats series detected in the image file metadata.
     """
     reader = ImageReader()
-    omeMeta = MetadataTools.createOMEXMLMetadata()
-    reader.setMetadataStore(omeMeta)
+    ome_meta = MetadataTools.createOMEXMLMetadata()
+    reader.setMetadataStore(ome_meta)
     reader.setId(path_to_file)
     series_count = reader.getSeriesCount()
     reader.close()
