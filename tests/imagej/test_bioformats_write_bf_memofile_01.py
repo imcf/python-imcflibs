4a92c094e90680fee209670f812915197af89998
kai.schleicher@unibas.ch <kai.schleicher@unibas.ch>
2023-04-18 15:20:20 +0200

    initial commit

---
+++ tests/imagej/test_bioformats_write_bf_memofile_01.py
@@ -0,0 +1,22 @@
+# @ (label="Select the python-imcf-libs testdata directory", style="directory") File PYTHON_IMCFLIBS_TESTDATA
+
+
+import os
+from imcflibs.imagej import bioformats
+
+
+parent_dir = str(PYTHON_IMCFLIBS_TESTDATA).replace("\\", "/")
+filename = "10x_phmax.czi"
+full_path = parent_dir + "/" + "10x_phmax.czi"
+bfmemofilename = "." + filename + ".bfmemo"
+os.chdir(parent_dir)
+if os.path.isfile(bfmemofilename):
+    print("bf memo file already existed and was removed")
+    os.remove(bfmemofilename)
+
+bioformats.write_bf_memoryfile(full_path)
+
+if not os.path.isfile(bfmemofilename):
+    print("test failed. A bf memo file does not exists")
+else:
+    print("test passed. A bf memo file was created.")
