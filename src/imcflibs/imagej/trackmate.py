8c485dad196080e0a7065fa399a377cd11f1b00a
Laurent Guerard <laurent.guerard@unibas.ch>
2023-07-04 16:01:26 +0200

    Clean up imports and code using settings as input

--- src/imcflibs/imagej/trackmate.py
+++ src/imcflibs/imagej/trackmate.py
@@ -1,18 +1,23 @@
+import os
 import sys
+
+from ij import IJ
 
 from fiji.plugin.trackmate import Logger, Model, SelectionModel, Settings, TrackMate
 from fiji.plugin.trackmate.action import LabelImgExporter
-from fiji.plugin.trackmate.cellpose import CellposeDetectorFactory
-from fiji.plugin.trackmate.cellpose.CellposeSettings import PretrainedModel
+from fiji.plugin.trackmate.cellpose import (
+    CellposeDetectorFactory,
+    LogDetectorFactory,
+    StarDistDetectorFactory,
+)
 from fiji.plugin.trackmate.features import FeatureFilter
-from fiji.plugin.trackmate.tracking.jaqaman import LAPUtils, SparseLAPTrackerFactory
+from fiji.plugin.trackmate.tracking import LAPUtils
+from fiji.plugin.trackmate.tracking.jaqaman import SparseLAPTrackerFactory
 from java.lang import Double
 
+from . import pathtools
 
-def run_tm(
-    implus,
-    detector_type,
-    channel_number,
+
 def cellpose_detector(
     imageplus,
     cellpose_env_path,
@@ -135,6 +140,7 @@ def log_detector(
 
     return settings
 
+
 def spot_filtering(
     settings,
     quality_thresh=None,
@@ -196,6 +202,7 @@ def spot_filtering(
 
     return settings
 
+
 def track_filtering(
     settings,
     link_max_dist=0.5,
@@ -243,6 +250,10 @@ def track_filtering(
 
     return settings
 
+
+def run_tm(
+    implus,
+    settings,
     crop_roi=None,
 ):
     # sourcery skip: merge-else-if-into-elif, swap-if-else-branches
@@ -250,22 +261,18 @@ def track_filtering(
 
     Parameters
     ----------
-    implus : ImagePlus
+    implus : ij.ImagePlus
         ImagePlus on which to run image
-    detector_type : str
-        Detector type to use
-    channel_number : int
-        Number of the channel of interest
-    quality_thresh : float, optional
-        Value to enter as threshold in the filters, by default None
-    intensity_thresh : float, optional
-        Value to enter as threshold in the filters, by default None
-    circularity_thresh : float, optional
-        Value to enter as threshold in the filters, by default None
-    area_thresh : float, optional
-        Value to enter as threshold in the filters, by default None
+    settings : fiji.plugin.trackmate.Settings
+        Settings to use for TrackMate
     crop_roi : ij.gui.Roi, optional
         ROI to crop on the image, by default None
+
+    Returns
+    -------
+    ij.ImagePlus
+        Labeled image with all the objects belonging to the same tracks having
+        the same label.
     """
 
     dims = implus.getDimensions()
@@ -281,56 +288,6 @@ def track_filtering(
 
     model.setLogger(Logger.IJTOOLBAR_LOGGER)
 
-    settings = Settings(implus)
-
-    set_detector(detector_type, channel_number, settings)
-    # settings.detectorFactory = CellposeDetectorFactory()
-
-    # settings.detectorSettings["TARGET_CHANNEL"] = nuclei_chnl
-    # settings.detectorSettings["OPTIONAL_CHANNEL_2"] = 0
-    # settings.detectorSettings["CELLPOSE_PYTHON_FILEPATH"] = os.path.join(
-    #     cellpose_env, "python.exe"
-    # )
-    # settings.detectorSettings["CELLPOSE_MODEL_FILEPATH"] = os.path.join(
-    #     os.environ["USERPROFILE"], ".cellpose", "models"
-    # )
-    # settings.detectorSettings["CELLPOSE_MODEL"] = PretrainedModel.CYTO2
-    # settings.detectorSettings["CELL_DIAMETER"] = 11.0
-    # settings.detectorSettings["USE_GPU"] = True
-    # settings.detectorSettings["SIMPLIFY_CONTOURS"] = True
-
-    # settings.addAllAnalyzers()
-    # spotAnalyzerProvider = SpotAnalyzerProvider(1)
-    # spotMorphologyProvider = SpotMorphologyAnalyzerProvider(1)
-
-    # #settings.initialSpotFilterValue=quality_thresh
-
-    # # Add the filter on mean intensity
-    # # Here 'true' takes everything ABOVE the mean_int value
-    if quality_thresh:
-        filter1 = FeatureFilter("QUALITY", Double(quality_thresh), True)
-        settings.addSpotFilter(filter1)
-    if intensity_thresh:
-        filter2 = FeatureFilter(
-            "MEAN_INTENSITY_CH" + str(channel_number), Double(intensity_thresh), True
-        )
-        settings.addSpotFilter(filter2)
-    if circularity_thresh:
-        filter3 = FeatureFilter("CIRCULARITY", Double(circularity_thresh), True)
-        settings.addSpotFilter(filter3)
-    if area_thresh:
-        filter4 = FeatureFilter("AREA", Double(area_thresh), False)
-        settings.addSpotFilter(filter4)
-
-    # Configure tracker
-    settings.trackerFactory = SparseLAPTrackerFactory()
-    settings.trackerSettings = settings.trackerFactory.getDefaultSettings()
-    # settings.addTrackAnalyzer(TrackDurationAnalyzer())
-    settings.trackerSettings["LINKING_MAX_DISTANCE"] = 15.0
-    settings.trackerSettings["GAP_CLOSING_MAX_DISTANCE"] = 15.0
-    settings.trackerSettings["MAX_FRAME_GAP"] = 3
-    settings.initialSpotFilterValue = -1.0
-
     trackmate = TrackMate(model, settings)
     trackmate.computeSpotFeatures(True)
     trackmate.computeTrackFeatures(True)
@@ -345,25 +302,32 @@ def track_filtering(
         if "[SparseLAPTracker] The spot collection is empty." in str(
             trackmate.getErrorMessage()
         ):
-            return IJ.createImage(
+            new_imp =  IJ.createImage(
                 "Untitled",
-                "8-bit black",
+                "16-bit black",
                 implus.getWidth(),
                 implus.getHeight(),
                 implus.getNFrames(),
             )
+            new_imp.setCalibration(cal)
+
+            return new_imp
+
         else:
             sys.exit(str(trackmate.getErrorMessage()))
+
             return
 
-    sm = SelectionModel(model)
+    SelectionModel(model)
 
     exportSpotsAsDots = False
     exportTracksOnly = False
     # implus2.close()
-    result = LabelImgExporter.createLabelImagePlus(
+    label_imp = LabelImgExporter.createLabelImagePlus(
         trackmate, exportSpotsAsDots, exportTracksOnly, False
     )
-    result.setDimensions(dims[2], dims[3], dims[4])
+    label_imp.setCalibration(cal)
+    label_imp.setDimensions(dims[2], dims[3], dims[4])
     implus.setDimensions(dims[2], dims[3], dims[4])
-    return result
+
+    return label_imp
