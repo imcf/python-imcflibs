eabcc44c90d1691f5b230c19ffb5cd5a0399a962
Kai Schleicher <kai.schleicher@unibas.ch>
2023-09-28 10:57:46 +0200

    Bugfix: fix option string in pairwise shift correlation

--- src/imcflibs/imagej/bdv.py
+++ src/imcflibs/imagej/bdv.py
@@ -395,7 +395,7 @@ def run_phase_correlation_pairwise_shifts_calculation(
 
     if downsampling_xyz != "":
         downsampling = (
-            "downsample_in_x=%s" + "downsample_in_y=%s downsample_in_z=%s ") % (
+            "downsample_in_x=%s downsample_in_y=%s downsample_in_z=%s ") % (
             downsampling_xyz[0],
             downsampling_xyz[1],
             downsampling_xyz[2]
