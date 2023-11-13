bba5cdfc112e8a5df2491175a169c152ff52710b
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2023-11-13 16:10:40 +0100

    Whitespace and quotes cleanup

--- tests/test_iotools.py
+++ tests/test_iotools.py
@@ -15,43 +15,43 @@ __license__ = "gpl3"
 
 
 def test_filehandle(tmpdir):
-    tmpfile = tmpdir.join('testfile')
+    tmpfile = tmpdir.join("testfile")
     tmpname = str(tmpfile)
-    tmphandle = open(str(tmpfile), 'w')
+    tmphandle = open(str(tmpfile), "w")
     print(type(tmphandle))
     assert type(tmpname) is str
     assert type(tmphandle) is file
     assert type(filehandle(tmpname)) is file
-    assert type(filehandle(tmphandle, 'w')) is file
+    assert type(filehandle(tmphandle, "w")) is file
 
 
 def test_readtxt(tmpdir):
     content = [
-        u'lorem\n',
-        u'ipsum\n',
-        u'and some more\n',
-        u'dummy text\n',
+        "lorem\n",
+        "ipsum\n",
+        "and some more\n",
+        "dummy text\n",
     ]
     fh = tmpdir.mkdir("readtxt").join("content.txt")
-    fh.write(u''.join(content))
+    fh.write("".join(content))
     print(fh.basename)
     print(fh.dirname)
-    with zipfile.ZipFile(join(fh.dirname, 'archive.zip'), 'w') as zf:
-        zf.write(str(fh), arcname='content.txt')
+    with zipfile.ZipFile(join(fh.dirname, "archive.zip"), "w") as zf:
+        zf.write(str(fh), arcname="content.txt")
         print("wrote [%s] into [%s]" % (str(fh), zf.filename))
-    
+
     print(content)
     fromfile = readtxt(str(fh))
     print(fromfile)
     assert fromfile == content
 
     fromfile_flat = readtxt(str(fh), flat=True)
-    assert fromfile_flat == ''.join(content)
+    assert fromfile_flat == "".join(content)
 
-    print(join(fh.dirname, 'archive.zip'))
-    fromzip = readtxt('content.txt', join(fh.dirname, 'archive.zip'))
+    print(join(fh.dirname, "archive.zip"))
+    fromzip = readtxt("content.txt", join(fh.dirname, "archive.zip"))
     print(fromzip)
     assert fromzip == content
 
-    fromzip_flat = readtxt('content.txt', join(fh.dirname, 'archive.zip'), flat=True)
-    assert fromzip_flat == ''.join(content)
+    fromzip_flat = readtxt("content.txt", join(fh.dirname, "archive.zip"), flat=True)
+    assert fromzip_flat == "".join(content)
