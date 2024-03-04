1d8f7b6b268f95ccc777c97cf1559f1b730e2c89
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2024-03-04 16:10:58 +0100

    Add test_processingoptions.py::test_defaults

--- tests/bdv/test_processingoptions.py
+++ tests/bdv/test_processingoptions.py
@@ -3,5 +3,33 @@ import pytest
 from imcflibs.imagej.bdv import ProcessingOptions
 
 
-def test_one():
+def test_defaults():
+    """Test the default options by calling all fomatters on a "raw" object."""
+    acitt_options = (
+        "process_angle=[All angles] "
+        "process_channel=[All channels] "
+        "process_illumination=[All illuminations] "
+        "process_tile=[All tiles] "
+        "process_timepoint=[All Timepoints] "
+    )
+    acitt_selectors = " "
+    use_acitt = (
+        "channels=[Average Channels] "
+        "illuminations=[Average Illuminations] "
+        "tiles=[Average Tiles] "
+        "timepoints=[Average Timepoints] "
+    )
+    how_to_treat = (
+        "how_to_treat_angles=[treat individually] "
+        "how_to_treat_channels=group "
+        "how_to_treat_illuminations=group "
+        "how_to_treat_tiles=group "
+        "how_to_treat_timepoints=group "
+    )
+
     proc_opts = ProcessingOptions()
+
+    assert proc_opts.fmt_acitt_options() == acitt_options
+    assert proc_opts.fmt_acitt_selectors() == acitt_selectors
+    assert proc_opts.fmt_use_acitt() == use_acitt
+    assert proc_opts.fmt_how_to_treat() == how_to_treat
