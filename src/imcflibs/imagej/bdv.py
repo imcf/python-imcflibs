391920b95b38122b88bd2420ecc1138aedaefd31
Laurent Guerard <laurent.guerard@unibas.ch>
2024-08-07 14:04:25 +0200

    Add global variables for new selection modes

--- src/imcflibs/imagej/bdv.py
+++ src/imcflibs/imagej/bdv.py
@@ -19,6 +19,8 @@ from .. import pathtools
 from ..log import LOG as log
 
 SINGLE = "[Single %s (Select from List)]"
+MULTIPLE = "[Multiple %ss (Select from List)] "
+RANGE = "[Range of %ss (Specify by Name)]"
 """@private template string"""
 
 
