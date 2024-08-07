51dcd21e3cf03662f9a575d16e15fb6d39f3a753
Laurent Guerard <laurent.guerard@unibas.ch>
2024-08-07 13:19:06 +0200

    Add test for example 2
    Example 2 is using a reference channel and a reference tile.

--- tests/bdv/test_processingoptions.py
+++ tests/bdv/test_processingoptions.py
@@ -69,6 +69,36 @@ def test__treat_tc_ti__ref_c1():
     assert proc_opts.fmt_use_acitt() == use_acitt
     assert proc_opts.fmt_how_to_treat() == how_to_treat
 
+def test__process_c1_treat_tg_ti_use_t3():
+    """Test an example setting using a reference channel and a reference tile."""
+
+    acitt_options = (
+        "process_angle=[All angles] "
+        "process_channel=[Single channel (Select from List)] "
+        "process_illumination=[All illuminations] "
+        "process_tile=[All tiles] "
+        "process_timepoint=[All Timepoints] "
+    )
+
+    acitt_selectors = "processing_channel=[channel 1] "
+    how_to_treat = (
+        "how_to_treat_angles=[treat individually] "
+        "how_to_treat_channels=group "
+        "how_to_treat_illuminations=group "
+        "how_to_treat_tiles=group "
+        "how_to_treat_timepoints=[treat individually] "
+    )
+    use_acitt = (
+        "channels=[Average Channels] "
+        "illuminations=[Average Illuminations] "
+        "tiles=[use Tile 3] "
+    )
+
+    proc_opts = ProcessingOptions()
+    proc_opts.process_channel(1)
+    proc_opts.treat_timepoints("[treat individually]")
+    proc_opts.reference_tile(3)
+
     assert proc_opts.fmt_acitt_options() == acitt_options
     assert proc_opts.fmt_acitt_selectors() == acitt_selectors
     assert proc_opts.fmt_use_acitt() == use_acitt
