d4ccdecdb7c1dac139c34901b2aa4b440c087710
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2024-11-14 12:02:52 +0100

    Improve pytest script output

--- scripts/py2-pytest.sh
+++ scripts/py2-pytest.sh
@@ -9,6 +9,7 @@ MOCKS_RELEASE="0.1.1"
 cd "$(dirname "$0")"/..
 
 test -d "venv2" || {
+    echo "== Creating a Python2 venv..."
     python2 -m virtualenv venv2
     source venv2/bin/activate
     URL_PFX="https://github.com/imcf/imcf-fiji-mocks/releases/download"
@@ -22,12 +23,25 @@ test -d "venv2" || {
         pip
 
     deactivate
+    echo "== Finished creating a Python2 venv."
 }
 
+echo "== Running 'poetry build'..."
 poetry build -vv
+echo "== Finished 'poetry build'."
+echo
 
+echo "== Installing local version of 'imcflibs' package..."
 venv2/bin/pip uninstall --yes imcflibs
 venv2/bin/pip install dist/*.whl
+echo "== Finished installing local 'imcflibs'."
+echo
 
-# call this script with `--cov --cov-report html` to generate coverage reports:
+echo "== Running pytest..."
+echo "$@" | grep -q -- "--cov" || {
+    echo "== NOTE: coverage reports will NOT be generated!"
+    echo "== Run with '--cov --cov-report html' to generate coverage reports!"
+}
+
+set -x
 venv2/bin/pytest "$@"
