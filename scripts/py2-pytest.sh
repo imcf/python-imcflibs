80a8d643e90dd6f7bca54d4705172ba4cf1a0993
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2024-11-19 23:35:34 +0100

    Use PyPi packages for building the Python2 venv

--- scripts/py2-pytest.sh
+++ scripts/py2-pytest.sh
@@ -3,27 +3,11 @@
 set -o errexit  # exit on any error
 set -o nounset  # empty variables are not permitted
 
-# FIXME: the MOCKS_RELEASE is now only used for DOWNLOADING the 'micrometa' and
-# 'sjlogging' wheels from github, this should be cleaned up and moved to PyPI as
-# well as soon as possible!
-
-# set the version of "imcf-fiji-mocks" to be used for downloading wheels:
-MOCKS_RELEASE="0.2.0"
-
 cd "$(dirname "$0")"/..
 
 test -d "venv2" || {
     echo "== Creating a Python2 venv..."
     python2 -m virtualenv venv2
-    URL_PFX="https://github.com/imcf/imcf-fiji-mocks/releases/download"
-    venv2/bin/pip --no-python-version-warning install --upgrade \
-        $URL_PFX/v$MOCKS_RELEASE/micrometa-15.2.2-py2.py3-none-any.whl \
-        sjlogging \
-        imcf-fiji-mocks \
-        olefile==0.46 \
-        pytest \
-        pytest-cov \
-        pip
     echo "== Finished creating a Python2 venv."
 
     echo "== Installing local version of 'imcflibs' package..."
@@ -50,6 +34,14 @@ setuptools.setup(
     rm -r src/imcflibs.egg-info
     echo "== Finished installing local 'imcflibs'."
     echo
+    venv2/bin/pip --no-python-version-warning install \
+        python-micrometa \
+        sjlogging \
+        "imcf-fiji-mocks>=0.3.0" \
+        olefile==0.46 \
+        pytest \
+        pytest-cov \
+        pip
 }
 
 echo "== Running pytest..."
