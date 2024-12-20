63101724eb4d9bed5c365136bc65f2d3fb552616
Rohan Girish <rohangirishrao@gmail.com>
2024-12-20 10:56:34 +0100

    Add reading addresses from Prefs and print errors
    Method now checks IJ preferences that has to be configured with a sender email address and an SMTP server. If either isn't present, print an error message and stop execution.

--- src/imcflibs/imagej/misc.py
+++ src/imcflibs/imagej/misc.py
@@ -7,7 +7,7 @@ import smtplib
 from ij import IJ  # pylint: disable-msg=import-error
 from ij.plugin.frame import RoiManager  # pylint: disable-msg=import-error
 from ij.measure import ResultsTable  # pylint: disable-msg=import-error
-
+from ij import Prefs as prefs
 from ..log import LOG as log
 
 
@@ -137,59 +137,59 @@ def find_focus(imp):
 
 
 def send_mail(job_name, recipient, filename, total_execution_time):
-    """Send an email via smtp.unibas.ch.
-    Will likely NOT work without connection to the unibas network.
+    """Send an email using the SMTP server and sender email configured in ImageJ's Preferences.
 
     Parameters
     ----------
-    recipient : string
-        recipients email address
     job_name : string
-        Job name to display in the email
+        Job name to display in the email.
+    recipient : string
+        Recipient's email address.
     filename : string
-        the name of the file to be passed in the email
+        The name of the file to be passed in the email.
     total_execution_time : str
-        the time it took to process the file in the format [HH:MM:SS:ss]
+        The time it took to process the file in the format [HH:MM:SS:ss].
     """
-    server = prefs.get("imcf.smtpserver", None)
-    if server is None:
-        print(
-            "Mail notifications only works if configured. Please use Plugins/IMCF_Utilities"
-        )
+    # Retrieve sender email and SMTP server from preferences
+    sender = prefs.get("imcf.sender_email", "").strip()
+    server = prefs.get("imcf.smtpserver", "").strip()
+
+    # Ensure the sender and server are configured from Prefs
+    if not sender:
+        print("Sender email is not configured. Please check IJ_Prefs.txt.")
+        return
+    if not server:
+        print("SMTP server is not configured. Please check IJ_Prefs.txt.")
         return
 
-    sender = prefs.get("imcf.sender_email", "")
-    if sender is "":
-        print(
-            "Mail notifications only works if configured. Please use Plugins/IMCF_Utilities"
-        )
+    # Ensure the recipient is provided
+    if not recipient.strip():
+        print("Recipient email is required.")
         return
 
-    header = "From: %s\n"
-    header += "To: %s\n"
-    header += "Subject: Your %s job finished successfully\n\n"
-    text = (
+    # Form the email subject and body
+    subject = "Your {0} job finished successfully".format(job_name)
+    body = (
         "Dear recipient,\n\n"
         "This is an automated message.\n"
-        "Your dataset %s has been successfully processed (%s [HH:MM:SS:ss]).\n\n"
+        "Your dataset '{0}' has been successfully processed "
+        "({1} [HH:MM:SS:ss]).\n\n"
         "Kind regards,\n"
         "The IMCF-team"
+    ).format(filename, total_execution_time)
+
+    # Form the complete message
+    message = ("From: {0}\nTo: {1}\nSubject: {2}\n\n{3}").format(
+        sender, recipient, subject, body
     )
 
-    message = header + text
-
+    # Try sending the email, print error message if it wasn't possible
     try:
         smtpObj = smtplib.SMTP(server)
-        smtpObj.sendmail(
-            sender,
-            job_name,
-            recipient,
-            message % (sender, recipient, job_name, filename, total_execution_time),
-        )
+        smtpObj.sendmail(sender, recipient, message)
         print("Successfully sent email")
-    except smtplib.SMTPException:
-        print("Error: unable to send email")
-
+    except smtplib.SMTPException as err:
+        print("Error: Unable to send email:", err)
     return
 
 
