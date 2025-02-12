b84ffeb1a744b7bae06bd08c0276614ab967460f
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2025-02-12 14:43:42 +0100

    Add script to parse POM and update pyproject.toml et al

--- scripts/run-poetry.sh
+++ scripts/run-poetry.sh
@@ -29,33 +29,10 @@ fi
 ### clean up old poetry artifacts:
 rm -rf dist/
 
-### parse the version from 'pom.xml':
-PACKAGE_VERSION=$(xmlstarlet sel --template -m _:project -v _:version pom.xml)
-PACKAGE_NAME=$(xmlstarlet sel --template -m _:project -v _:artifactId pom.xml)
-PACKAGE_DIR="src/${PACKAGE_NAME#python-}"  # strip 'python-' prefix if present
+# adjust metadata version strings:
+scripts/pom-to-pyproject.sh
 
-echo "Package version from POM: [$PACKAGE_VERSION]"
-### make sure to have a valid Python package version:
-case $PACKAGE_VERSION in
-*-SNAPSHOT*)
-    PACKAGE_VERSION=${PACKAGE_VERSION/-SNAPSHOT/}
-    ### calculate the distance to the last release tag:
-    LAST_TAG=$(git tag --list "${PACKAGE_NAME}-*" | sort | tail -n1)
-    # echo "Last git tag: '$LAST_TAG'"
-    COMMITS_SINCE=$(git rev-list "${LAST_TAG}..HEAD" | wc -l)
-    # echo "Nr of commits since last tag: $COMMITS_SINCE"
-    HEAD_ID=$(git rev-parse --short HEAD)
-    # echo "HEAD commit hash: $HEAD_ID"
-    PACKAGE_VERSION="${PACKAGE_VERSION}.dev${COMMITS_SINCE}+${HEAD_ID}"
-    ;;
-esac
-
-echo "Using Python package version: [$PACKAGE_VERSION]"
-### put the version into the project file and the package source:
-sed -i "s/\"0.0.0\"/\"${PACKAGE_VERSION}\"/" pyproject.toml
-sed -i "s/\${project.version}/${PACKAGE_VERSION}/" "${PACKAGE_DIR}/__init__.py"
-
-sed -i 's/^python = ">=2.7"$/python = ">=3.10"/' pyproject.toml
+# # sed -i 's/^python = ">=2.7"$/python = ">=3.10"/' pyproject.toml
 
 set +o errexit  # otherwise the script stops if poetry exits non-zero
 poetry "$@"  # run poetry with the parameters given to the script
@@ -63,6 +40,6 @@ RETVAL=$?  # remember the actual exit code of poetry for returning it below!
 
 ### clean up the moved source tree and restore the previous state:
 git restore pyproject.toml
-git restore "${PACKAGE_DIR}/__init__.py"
+git restore "src/*/__init__.py"
 
 exit $RETVAL  # now return the exit code from running poetry
