6e9f8a10284acd4ede683c73c7ae7f7c0b4aad80
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2025-02-13 14:35:51 +0100

    Modify Python version dependency by default
    This is required for all "complex" Poetry operations, e.g. when
    installing the project to run pytest or similar. To allow for building
    packages that can be installed on Python 2, this can be turned off
    explicitly by setting the environment variable IGNORE_DEPS_PYTHON.

--- scripts/run-poetry.sh
+++ scripts/run-poetry.sh
@@ -27,7 +27,19 @@ rm -rf dist/
 # adjust metadata version strings:
 scripts/pom-to-pyproject.sh
 
-# # sed -i 's/^python = ">=2.7"$/python = ">=3.10"/' pyproject.toml
+# in case the project needs to be "installed" (e.g. to run pytest and generate
+# coverage reports), the Python version specified in the project's dependencies
+# needs to be set to a version satisfying the other dependencies requirements -
+# this can be turned off e.g. for simply packaging for PyPi.
+PYPROJ="pyproject.toml"
+DEPS_PYTHON='>=3.10'
+if [ -z "$IGNORE_DEPS_PYTHON" ]; then
+    echo "$PYPROJ: setting [python = \"$DEPS_PYTHON\"]"
+    echo "Use 'export IGNORE_DEPS_PYTHON=true' to skip this step."
+    sed -i 's/^python = ">=2.7"$/python = "'"$DEPS_PYTHON"'"/' $PYPROJ
+else
+    echo "$PYPROJ: found 'IGNORE_DEPS_PYTHON' envvar, not modifying."
+fi
 
 set +o errexit  # otherwise the script stops if poetry exits non-zero
 poetry "$@"  # run poetry with the parameters given to the script
