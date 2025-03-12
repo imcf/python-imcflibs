19f82c71b9139781cb96551a625d55986a530da8
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2025-03-12 18:38:36 +0100

    Clean up unused imports in the trackmate module

--- src/imcflibs/imagej/trackmate.py
+++ src/imcflibs/imagej/trackmate.py
@@ -8,7 +8,7 @@ from fiji.plugin.trackmate.cellpose.CellposeSettings import PretrainedModel
 from fiji.plugin.trackmate.detection import LogDetectorFactory
 from fiji.plugin.trackmate.features import FeatureFilter
 from fiji.plugin.trackmate.stardist import StarDistDetectorFactory
-from fiji.plugin.trackmate.tracking.jaqaman import LAPUtils, SparseLAPTrackerFactory
+from fiji.plugin.trackmate.tracking.jaqaman import SparseLAPTrackerFactory
 
 from ij import IJ
 
