edbef7da6a2ddf6d702da49024c380d006e11997
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2025-03-14 00:11:22 +0100

    Add docstrings to tests

--- tests/test_strtools.py
+++ tests/test_strtools.py
@@ -11,24 +11,29 @@ from imcflibs.strtools import strip_prefix
 
 
 def test__is_string_like():
+    """Test `_is_string_like()`."""
     assert _is_string_like("foo") == True
     assert _is_string_like(12345) == False
 
 
 def test_filename_from_string():
+    """Test `filename()` using a string."""
     assert filename("test_file_name") == "test_file_name"
 
 
 def test_filename_from_handle(tmpdir):
+    """Test `filename()` using a file handle."""
     path = str(tmpdir)
     fhandle = tmpdir.join("foo.txt")
     assert filename(fhandle) == os.path.join(path, "foo.txt")
 
 
 def test_flatten():
+    """Test `flatten()` using a tuple."""
     assert flatten(("foo", "bar")) == "foobar"
 
 
 def test_strip_prefix():
+    """Test `strip_prefix()`."""
     assert strip_prefix("foobar", "foo") == "bar"
     assert strip_prefix("foobar", "bar") == "foobar"
