0d02cafe8e1cc32da1ba69cbcb5569bcc9987279
Laurent Guerard <laurent.guerard@unibas.ch>
2025-03-27 15:19:09 +0100

    Move `pad_number()` to `strtools`

--- src/imcflibs/imagej/misc.py
+++ src/imcflibs/imagej/misc.py
@@ -627,31 +627,6 @@ def save_image_in_format(imp, format, out_dir, series, pad_number, split_channel
         current_imp.close()
 
 
-def pad_number(index, pad_length=2):
-    """Pad a number with leading zeros to a specified length.
-
-    Parameters
-    ----------
-    index : int or str
-        The number to be padded
-    pad_length : int, optional
-        The total length of the resulting string after padding, by default 2
-
-    Returns
-    -------
-    str
-        The padded number as a string
-
-    Examples
-    --------
-    >>> pad_number(7)
-    '07'
-    >>> pad_number(42, 4)
-    '0042'
-    """
-    return str(index).zfill(pad_length)
-
-
 def locate_latest_imaris(paths_to_check=None):
     """Find paths to latest installed Imaris or ImarisFileConverter version.
 
