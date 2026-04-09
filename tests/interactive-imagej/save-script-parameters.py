0401201a63c04d99c53c6340f411fb490eb72fd0
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2026-04-09 13:34:03 +0200

    Align with testing instructions
    The workflow described in TESTING.md states to drag and drop the script
    onto Fiji, so for the time being let's stick to that

---
+++ tests/interactive-imagej/save-script-parameters.py
@@ -0,0 +1,20 @@
+# @ String(label="Username") USERNAME
+# @ String(label="Password", style="password") PASSWORD
+# @ File(label="Path for results", style="directory") outputPath
+# @ Integer threshold
+# @ Boolean(label="Yes/No?") choice
+# @ RoiManager rm
+# @ CommandService command
+# @ LogService sjlog
+
+import os
+
+import imcflibs.log
+from imcflibs.imagej import misc
+
+imcflibs.log.enable_console_logging()
+log = imcflibs.log.LOG
+
+log.warning("Starting...")
+misc.save_script_parameters(script_globals=globals(), destination=outputPath)
+log.warning("Saved parameters to: %s/script_parameters.txt", outputPath)
