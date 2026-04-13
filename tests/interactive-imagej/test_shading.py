eb8cd6fd066ee2d9e6c6d49dd83005542660dabf
Rohan Girish <rohangirishrao@gmail.com>
2026-04-13 14:27:40 +0200

    Clean up comment

--- tests/interactive-imagej/test_shading.py
+++ tests/interactive-imagej/test_shading.py
@@ -1,10 +1,7 @@
 # Prerequisites before running this script in Fiji:
 #
-# - The shading branch's .jar must be placed into the ./jars folder of your
-#   Fiji installation.
 # - Open an image to test on (e.g. Shaded-blobs.png) in Fiji before running.
-# - Drag this script into Fiji and run it, or create a new script with language
-#   set to Python and paste the contents.
+# - Drag this script into Fiji and run it.
 #
 # Expected result: a new image titled "Result of blobs.gif" opens alongside
 # the original, showing a flatfield corrected version of the raw blobs.gif.
